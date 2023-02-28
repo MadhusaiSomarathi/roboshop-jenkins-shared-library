4f6605a1357d5b288bfa4a2700c342eeb87aa951
rthootika <msamrat789@gmail.com>
2023-02-28 11:46:01 +0530

    updates for cart

--- vars/common.groovy
+++ vars/common.groovy
@@ -9,7 +9,7 @@ def codeQuality() {
     stage('Code Quality') {
         withCredentials([usernamePassword(credentialsId: 'SONAR', passwordVariable: 'sonarPass', usernameVariable: 'sonarUser')]) {
             sh '''
-        sonar-scanner -Dsonar.host.url=http://172.31.9.128:9000 -Dsonar.login=${sonarUser} -Dsonar.password=${sonarPass} -Dsonar.projectKey=${COMPONENT} -Dsonar.qualitygate.wait=true
+        sonar-scanner -Dsonar.host.url=http://172.31.28.6:9000 -Dsonar.login=${sonarUser} -Dsonar.password=${sonarPass} -Dsonar.projectKey=${COMPONENT} -Dsonar.qualitygate.wait=true
         echo OK
       '''
         }
